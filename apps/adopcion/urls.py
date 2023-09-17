a8c27848ee086cd963ad7bad1aadb4aca4c8ba03
Luis Angel <luisangel.fnz@gmail.com>
2023-09-16 18:41:17 -0600

    Agregar funionalidad al modulo adopcion/solicitud/eliminar

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from apps.adopcion.views import index_adopcion
 
-from apps.adopcion.views import SolicitudList, SolicitudCreate
+from apps.adopcion import views
+
 app_name = 'adopcion'
 
 urlpatterns = [
-    path('', index_adopcion),
-    path('solicitud', SolicitudCreate.as_view(), name='solicitud'),
-    path('listar', SolicitudList.as_view(), name='listar_solicitud'),
+    path('', views.index_adopcion),
+    path('solicitud', views.SolicitudCreate.as_view(), name='solicitud'),
+    path('solicitud/listar', views.SolicitudList.as_view(), name='listar_solicitud'),
+    path('solicitud/editar/<int:pk>', views.SolicitudUpdate.as_view(), name='editar_solicitud'),
+    path('solicitud/eliminar/<int:pk>', views.SolicitudDelete.as_view(), name='eliminar_solicitud')
 ]
--- apps/adopcion/views.py
+++ apps/adopcion/views.py
@@ -1,29 +1,33 @@
 from typing import Any
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
 
 from django.views.generic import (
     ListView, CreateView, UpdateView, DeleteView
 )
 
-from apps.adopcion.models import Solicitud
+from apps.adopcion.models import Solicitud,Persona
 from apps.adopcion.forms  import SolicitudForm, PersonaForm
 # Create your views here.
 
 def index_adopcion(request):
     return HttpResponse('Hola Mundo desde la app adopcion')
 
-class SolicitudList(ListView):
+class SolicitudBase:
     model = Solicitud
+    success_url   = reverse_lazy('adopcion:listar_solicitud')
+
+class SolicitudList(SolicitudBase, ListView):
     template_name = 'tadopcion/solicitud_listar.html'
 
-class SolicitudCreate(CreateView):
-    model         = Solicitud
+class SolicitudCU(SolicitudBase):    
     template_name = 'tadopcion/solicitud_form.html'
     form_class    = SolicitudForm
     form_persona  = PersonaForm
-    success_url   = reverse_lazy('adopcion:listar_solicitud')
+    
+
+class SolicitudCreate(SolicitudCU,CreateView):
     
     def post(self, request, *args, **kwargs):
         self.object = self.get_object
@@ -51,3 +55,36 @@ class SolicitudCreate(CreateView):
             context['frm_persona'] = self.form_persona(self.request.GET) # type: ignore
         
         return context
+
+class SolicitudUpdate(SolicitudCU, UpdateView):
+    model_persona = Persona
+    
+    def get_context_data(self, **kwargs):
+        context = super(SolicitudUpdate, self).get_context_data(**kwargs)
+        solicitud_id  = self.kwargs.get('pk', 0)
+        solicitud     = self.model.objects.get(id=solicitud_id)
+        persona       = self.model_persona.objects.get(id=solicitud.persona_id) #type: ignore
+        if 'form' not in context:
+            context['form'] = self.form_class() #type: ignore
+        
+        if 'frm_persona' not in context:
+            context['frm_persona'] = self.form_persona(instance=persona)
+        
+        context['id'] = solicitud_id
+        return context
+     
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object
+        solicitud_id = kwargs['pk']
+        solicitud = self.model.objects.get(id=solicitud_id)
+        persona = self.model_persona.objects.get(id=solicitud.persona_id) #type: ignore
+        frm_solicitud = self.form_class(request.POST, instance=solicitud) #type: ignore
+        frm_persona = self.form_persona(request.POST, instance=persona)
+        if frm_solicitud.is_valid() and frm_persona.is_valid():
+            frm_solicitud.save() #type: ignore
+            frm_persona.save()
+            # return HttpResponseRedirect(self.get_success_url())
+        return HttpResponseRedirect(self.get_success_url())
+
+class SolicitudDelete(SolicitudBase,DeleteView):
+    template_name = 'tadopcion/solicitud_delete.html'
