9c10dcd220f49a17f3bceb42b3b53d24611ae0e7
Luis Angel <luisangel.fnz@gmail.com>
2023-09-29 00:04:13 -0600

    Capitulo 25(final) introduccion a Serializers aplicada en usuario

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from apps.usuario.views import RegistroUsuario
+from apps.usuario.views import RegistroUsuario, UserAPI
 
 app_name = 'usuario'
 
 urlpatterns = [
-    path('registrar', login_required(RegistroUsuario.as_view()), name='registrar_usuario')
+    path('registrar', login_required(RegistroUsuario.as_view()), name='registrar_usuario'),
+    path('api', UserAPI.as_view(), name='usuario_api')
 ]
--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+
 from apps.usuario.forms import UsuarioForm
+from apps.usuario.serializers import UserSerializer
+from rest_framework.views import APIView
+from django.http import HttpResponse
 
 class RegistroUsuario(CreateView):
     model         = User
     template_name = 'tusuario/registrar.html'
     form_class    = UsuarioForm
     success_url   = reverse_lazy('mascota:listar_mascotas')
+
+
+class UserAPI(APIView):
+    serializer = UserSerializer
+
+    def get(self, request, format=None):
+        users = User.objects.all()
+        response = self.serializer(users, many=True)
+        return HttpResponse(json.dumps(response.data), content_type='application/json')
